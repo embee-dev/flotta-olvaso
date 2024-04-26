d958ba2dfb82f6ea7cfb8d62b7950942b76955a0
embee-dev <balazs.z.marton.dev@gmail.com>
2024-04-26 15:04:19 +0200

    create first test publish on Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/flotta-olvaso/',
   plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
